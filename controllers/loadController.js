ee5983fd8d908c622ccc7c948ab1ae6fbafc9637
Pablo Rojas <projasb25@gmail.com>
2021-12-14 23:26:37 -0500

    guide_number

--- controllers/loadController.js
+++ controllers/loadController.js
@@ -51,6 +51,10 @@ exports.insert = async (req, res) => {
                 item.client_date2 = new Date(Math.round((item.client_date2 - 25569)*86400*1000));
             }
 
+            if (!('guide_number' in item)) {
+                item.guide_number = item.seg_code;
+            }
+
             item.massiveloadid = result[0];
             item.seg_code = item.seg_code || null;
             item.guide_number = item.guide_number || null;
