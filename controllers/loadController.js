aa2e543cf8b2f22843424dd4cc737129ae295335
Carlos Farro <fdcarlosd1@gmail.com>
2022-01-20 07:10:57 -0500

    fix

--- controllers/loadController.js
+++ controllers/loadController.js
@@ -114,7 +114,7 @@ exports.insert = async (req, res) => {
                 transaction
             }).catch(err => {
                 lasterror = getErrorSeq(err);
-                throw (lasterror.message || 'error') + " EN LA FILA + " + index + 1
+                throw (lasterror.message || 'error') + " EN LA FILA " + index + 1
             });
         }))
 
